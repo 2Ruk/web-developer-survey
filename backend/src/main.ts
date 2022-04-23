c158f4a94ef4954237f5dc17f30f132c28aaabec
2Ruk <leerukdev@gmail.com>
2022-04-23 19:17:18 +0900

    project default setting 1. backend   - lib 설치

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,13 +5,15 @@ import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('survey-apis')
+    .setDescription('The web-developer-survey API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('doc')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('doc', app, document);
+
+  app.setGlobalPrefix('/api')
   await app.listen(3000);
 }
 bootstrap();
