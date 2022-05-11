70d05bac6764f40e521c93ad1b87ad97cf3a2159
2Ruk <leerukdev@gmail.com>
2022-05-12 00:48:35 +0900

    Common - 프로젝트 시작 전 세팅 점검

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,42 +1,35 @@
 <template>
-  <v-container fluid="fluid">
-    <v-row>
-      <v-col cols="12" class="align-center" >
-        <v-card outlined>
-          <v-img
-              class="ma-auto"
-              src="https://pbs.twimg.com/media/DP8PRviVQAA6oJZ?format=png"
-              lazy-src="https://pbs.twimg.com/media/DP8PRviVQAA6oJZ?format=jpeg"
-              max-width="500"
-              max-height="500"/>
-
-        </v-card>
-        <v-spacer/>
-        <v-card>
-          <v-list
-              dense
-              nav
-          >
-            <v-list-item
-                v-for="item in items"
-                :key="item.title"
-                link
-                :href="item.href"
-            >
-              <v-list-item-icon>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-card>
-      </v-col>
-      <v-col cols="12">
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-main class="grey lighten-5">
+    <v-container class="justify-center" >
+      <v-card outlined width="1000" elevation="1" class="ma-auto">
+        <v-img
+          class="ma-auto "
+          src="https://pbs.twimg.com/media/DP8PRviVQAA6oJZ?format=png"
+          lazy-src="https://pbs.twimg.com/media/DP8PRviVQAA6oJZ?format=jpeg"
+          max-width="500"
+          max-height="500"/>
+        <v-divider class="mx-4 ma-2"></v-divider>
+        <v-list
+          dense
+          nav
+        >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :href="item.href"
+        >
+        <v-list-item-icon>
+        <v-icon>{{item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+        </v-list-item>
+        </v-list>
+      </v-card>
+    </v-container >
+  </v-main>
 </template>
 
 <script lang="ts">
